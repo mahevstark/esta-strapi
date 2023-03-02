98911af59a73b09e2e272444bfc3c295c630b685
mahevstark <mahevstark@gmail.com>
2023-03-02 11:41:47 +0500

    push

--- src/api/guest/controllers/guest.js
+++ src/api/guest/controllers/guest.js
@@ -6,4 +6,33 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::guest.guest');
+module.exports = createCoreController('api::guest.guest' , ({strapi})=>({
+
+    async create(ctx){
+
+        // check if guest already exists using device_id
+
+        let device_id = ctx.request.body.data.device_id;
+        let guest = await strapi.query('api::guest.guest').findOne({where:{device_id}});
+
+        if(guest){
+            console.log('guest already exists');
+
+            // update guest
+
+
+            let gg = await strapi.query('api::guest.guest').update(
+                {
+                    where:{device_id},
+                    data:ctx.request.body.data
+                }
+            );
+
+            return gg;
+        }
+
+
+        let data = await strapi.query('api::guest.guest').create(ctx.request.body);
+        return data;
+    }
+}));
