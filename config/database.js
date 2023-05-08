919dc98f8ef69f4938c50e1caaaeb3f827bbedcd
mahevstark <39601038+mahevstark@users.noreply.github.com>
2023-05-08 19:00:50 +0500

    Update database.js

--- config/database.js
+++ config/database.js
@@ -11,12 +11,12 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'mysql',
     connection: {
-      host:  'containers-us-west-59.railway.app',
-      port: 7385,
-      database: 'estacar',
-      user: 'esta',
-      password:  'Faisal284hr%%',
-      ssl:  true,
+      host: env('DATABASE_HOST', 'localhost'),
+      port: env.int('DATABASE_PORT', 3306),
+      database: env('DATABASE_NAME', 'estav2'),
+      user: env('DATABASE_USERNAME', 'root'),
+      password: env('DATABASE_PASSWORD', 'mysql'),
+      ssl: env.bool('DATABASE_SSL', true),
     },
   },
 });
