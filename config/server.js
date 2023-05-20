593700eeb2456ce6ca19062e3ee6312eb6fcbe37
mahevstark <39601038+mahevstark@users.noreply.github.com>
2023-05-20 22:00:39 +0500

    Update server.js

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1700),
-  url: env('URL', 'http://192.168.18.3:1700'),
+  port: env.int('PORT', 1337),
+  // url: env('URL', 'http://192.168.18.3:1700'),
   app: {
     keys: env.array('APP_KEYS'),
   },
